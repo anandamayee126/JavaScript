1fc028ce5c574d5f41ba1b7a56d2203d5192f6f0
Anandamayee Ghosh <ghosh.anandamayee>
2023-08-12 22:29:06 +0530

    index and main js added

---
+++ main.js
@@ -0,0 +1,8 @@
+var title1= document.getElementById('title-id');
+title1.style.border = '3px solid black';
+
+var title2= document.getElementsByClassName('title');
+title2.style.display = 'block';
+
+var title3= document.getElementsByTagName('li');
+title3.style.color = 'green';
