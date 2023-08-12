5efe025f5fd58b1f21c5df3ca07c64418a5e1330
Anandamayee Ghosh <ghosh.anandamayee>
2023-08-12 23:56:11 +0530

    font color of the items has been changed

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
-var title1= document.getElementById('title-id');
-title1.style.border = '3px solid black';
+// var title1= document.getElementById('title-id');
+// title1.style.border = '3px solid black';
 
-var title2= document.getElementsByClassName('title');
-title2.style.display = 'block';
+// var title2= document.getElementsByClassName('title');
+// title2.style.display = 'block';
 
-var title3= document.getElementsByTagName('li');
-title3.style.color = 'green';
+const ulParent= document.querySelector('ul');
+let ulChild=ulParent.children;
+ulChild[0].style.color = 'green';
+ulChild[1].style.color = 'green';
+ulChild[2].style.color = 'green';
+ulChild[3].style.color = 'green';
