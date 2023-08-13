508e6b3361da71df4865767ddca747e1c9b1d806
Anandamayee Ghosh <ghosh.anandamayee>
2023-08-13 10:00:16 +0530

    tag name

--- main.js
+++ main.js
@@ -4,15 +4,17 @@
 // var title2= document.getElementsByClassName('title');
 // title2.style.display = 'block';
 
-const list= document.getElementsByClassName('list-group-item')
-//let ulChild=ulParent.children;
-for(var i=0;i<list.length;i++) {
-    list[i].style.color='green';
-    list[i].style.fontWeight='bold';
-}
-// ulChild[0].style.color = 'green';
-// ulChild[1].style.color = 'green';
-// ulChild[2].style.backgroundColor = 'green';
-// ulChild[3].style.color = 'green';
+// const list= document.getElementsByClassName('list-group-item')
+// //let ulChild=ulParent.children;
+// for(var i=0;i<list.length;i++) {
+//     list[i].style.color='green';
+//     list[i].style.fontWeight='bold';
+// }
 
+const li= document.getElementsByTagName('li')
+//let ulChild=ulParent.children;
+for(var i=0;i<li.length;i++) {
+    li[i].style.color='green';
+    li[i].style.fontWeight='bold';
+}
 
